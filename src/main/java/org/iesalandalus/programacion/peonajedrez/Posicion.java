6adbb32dbcb82b7db5faf8f564a2a52057be4b62
AntonioAlonso-Tec <56316349+AntonioAlonso-Tec@users.noreply.github.com>
2019-10-31 17:51:00 +0100

    Update Posicion.java
    Añadidos getter y setter

--- src/main/java/org/iesalandalus/programacion/peonajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/peonajedrez/Posicion.java
@@ -3,4 +3,25 @@ package org.iesalandalus.programacion.peonajedrez;
 public class Posicion {
 	private int fila;
 	private char columna;
+	public int getFila() {
+		return fila;
+	}
+	public void setFila(int fila) {
+		
+		if (fila<1 || fila>8) {
+			throw new IllegalArgumentException("El numero de fila no es correcto");
+		}else {
+			this.fila = fila;
+		}
+	}
+	public char getColumna() {
+		return columna;
+	}
+	public void setColumna(char columna) {
+		if (columna<'a'&&columna>'h') {
+			throw new IllegalArgumentException("La columna no es correcta");
+		}else {
+			  this.columna = columna;
+		}
+	}
 }
