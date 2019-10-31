54982508c7847132fc1cfd74213dc471b0ba942b
AntonioAlonso-Tec <56316349+AntonioAlonso-Tec@users.noreply.github.com>
2019-10-31 18:24:37 +0100

    Update Posicion.java
    Creacion del primer constructor

--- src/main/java/org/iesalandalus/programacion/peonajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/peonajedrez/Posicion.java
@@ -3,10 +3,20 @@ package org.iesalandalus.programacion.peonajedrez;
 public class Posicion {
 	private int fila;
 	private char columna;
+	
+	public Posicion(int fila,char columna){
+		if (fila<1||fila>8 && columna<'a'||columna>'h') {
+			throw new IllegalArgumentException("La posicion en el tablero no es correcta");
+		}else {
+			setFila(fila);
+			setColumna(columna);
+		}
+	}
+	
 	public int getFila() {
 		return fila;
 	}
-	public void setFila(int fila) {
+	private void setFila(int fila) {
 		
 		if (fila<1 || fila>8) {
 			throw new IllegalArgumentException("El numero de fila no es correcto");
@@ -17,7 +27,7 @@ public class Posicion {
 	public char getColumna() {
 		return columna;
 	}
-	public void setColumna(char columna) {
+	private void setColumna(char columna) {
 		if (columna<'a'&&columna>'h') {
 			throw new IllegalArgumentException("La columna no es correcta");
 		}else {
