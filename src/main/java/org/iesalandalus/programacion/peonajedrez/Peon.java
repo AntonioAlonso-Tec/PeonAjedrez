a8e95bcec4f417396d083ad7ae04d05f5d97dc53
AntonioAlonso-Tec <56316349+AntonioAlonso-Tec@users.noreply.github.com>
2019-11-02 15:50:02 +0100

    Update Peon.java
    Creados getter y setter

--- src/main/java/org/iesalandalus/programacion/peonajedrez/Peon.java
+++ src/main/java/org/iesalandalus/programacion/peonajedrez/Peon.java
@@ -3,4 +3,28 @@ package org.iesalandalus.programacion.peonajedrez;
 public class Peon {
 	private Color color;
 	private Posicion posicion;
+	
+	public Color getColor() {
+		return color;
+	}
+	private void setColor(Color color) {
+		
+	if (color!=Color.BLANCO&&color!=Color.NEGRO) {
+		throw new IllegalArgumentException("El peon solo puede ser blanco o negro");
+	}else {
+		this.color = color;
+	}
+	}
+	public Posicion getPosicion() {
+		return posicion;
+	}
+	private void setPosicion(Posicion posicion) {
+		if (posicion.getFila()<1||posicion.getFila()>8) {
+			throw new IllegalArgumentException("El numero de fila es incorecto");
+		}else if(posicion.getColumna()<'a'||posicion.getColumna()>'h') {
+			throw new IllegalArgumentException("Esta columna no existe");
+		}else {
+			this.posicion = posicion;
+		}
+	}
 }
