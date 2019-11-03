f0602456a278aa1dad87e7c7cbf0370e6d7bc8cf
AntonioAlonso-Tec <56316349+AntonioAlonso-Tec@users.noreply.github.com>
2019-11-03 12:14:50 +0100

    Update MainApp.java
    Creado metodo mostrarPeon (vacío)

--- src/main/java/org/iesalandalus/programacion/peonajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/peonajedrez/MainApp.java
@@ -1,10 +1,19 @@
 package org.iesalandalus.programacion.peonajedrez;
 
 public class MainApp {
+	Peon peon;
+	
+	private static void mostrarPeon() {
+		
+	}
 	
 	public static void main(String[] args) {
+	
+		
 		System.out.println("Programa para aprender a colocar y mover un peón en el tablero de ajedrez");
 		System.out.println("-------------------------------------------------------------------------");
+		
+		
 		
 		System.out.println("Hasta luego Lucas!!!!");
 	}
