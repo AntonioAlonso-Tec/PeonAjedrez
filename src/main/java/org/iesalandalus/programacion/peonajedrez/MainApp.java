aa71d210221d9afd0fb8c07406596b0fc510784f
AntonioAlonso-Tec <56316349+AntonioAlonso-Tec@users.noreply.github.com>
2019-11-03 12:33:48 +0100

    Update MainApp.java
    Creado metodo mostrarMenu

--- src/main/java/org/iesalandalus/programacion/peonajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/peonajedrez/MainApp.java
@@ -7,6 +7,19 @@ public class MainApp {
 		
 	}
 	
+	private static void mostrarMenu() {
+		System.out.println("Vamos a proceder a crear un peon de ajedrez");
+		System.out.println("Escoge una opcion");
+		System.out.println("");
+		System.out.println("------------------------------------------------------------------------");
+		System.out.println("1.- CREAR UN PEON POR DEFECTO");
+		System.out.println("2.- ESCOGER COLOR: BLANCO/NEGRO");
+		System.out.println("3.- ESTABLECER COLUMNA INICIAL");
+		System.out.println("4.- MOSTRAR DATOS DEL PEON CREADO");
+		System.out.println("5.- MOVER PEON");
+		System.out.println("6.- SALIR");
+	}
+	
 	public static void main(String[] args) {
 	
 		
